e28308b57a56ae70d9d581dda26f2bf2a5a21de6
Amagin0 <amaginyos.ask.given.u@gmail.com>
2022-03-18 02:40:08 +0900

    [add]タブ切り替え完成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,7 +6,7 @@ class HomesController < ApplicationController
     @random_1st = Question.find(random_theme).first
     @random_2nd = Question.find(random_theme).second
     @popular_questions = Question.where(created_at: Time.current.all_month).limit(3).order(raty: :desc)
-    @new_questions = Question.limit(5).order(created_at: :desc)
+    @new_questions = Question.limit(3).order(created_at: :desc)
   end
 
   def about
